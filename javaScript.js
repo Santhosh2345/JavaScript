62d7a0dbe7a3525c0f29c89a86b4221bb88a518b
= <=>
2023-01-27 22:31:00 +0530

    innerhtml

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,19 @@
 console.log("Hello New World");
+function add(num){
+    var temp = num;
+    var sum =0;
+    while(temp>0){
+        var a = temp%10;
+        sum += (a*a*a);
+        temp=temp/10;  
+        temp = Math.floor(temp);
+    }
+    if(sum===num){
+    console.log("Given Number is a Amstrong");
+    }
+    if(sum!==num){
+        console.log("Given Number is a Not an Amstrong");
+        }
+}
+
+add(150)
